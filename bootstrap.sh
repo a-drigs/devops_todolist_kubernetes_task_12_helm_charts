232c59f0cb493d1306a580b512cac9b354b674bd
Andrii <a17drigs@gmail.com>
2026-02-26 04:23:38 +0200

    Solution (change dir for helm)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kind create cluster --config cluster.yaml
-helm install todoapp-release helm-chart/todoapp
+helm install todoapp-release .infrastructure/helm-chart/todoapp
 
 kubectl get all,cm,secret,ing -A
 # check all kube resources
